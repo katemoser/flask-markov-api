caf3ed10dc50d7640c2702b477018c32f69ad08b
Kate Moser <komoser@gmail.com>
2022-09-14 13:48:14 -0500

    Made it possible to feed several texts in as seed Must be deliniated by @ sign

--- Markov.py
+++ Markov.py
@@ -2,18 +2,33 @@ import random
 
 
 class MarkovMachine:
-    """Make a markov machine out of a string"""
+    """Make a markov machine out of a string
+    
+    Separate seeds must be separated by the "@" sign.
+    
+    """
 
     def __init__(self, text):
         self.words = text.split()
-        # self.first_words = []
+        self.first_words = []
         self.chains = self.get_chains()
     
     def get_chains(self):
         chains = {}
+        # add first word to first words
+        self.first_words.append(self.words[0])
         for idx, current_word in enumerate(self.words):
             if idx < len(self.words) - 1:
-                next_word = self.words[idx + 1] 
+                if self.words[idx + 1] == "@":
+                    # this is a terminal word. 
+                    # we want to push None to the chain
+                    # put the word after the @ into first words
+                    # push the index forward
+                    next_word = None
+                    self.first_words.append(self.words[idx + 2])
+                    idx += 1
+                else:
+                    next_word = self.words[idx + 1] 
             else:
                 next_word = None
 
@@ -23,10 +38,12 @@ class MarkovMachine:
         return chains
 
     def get_text(self):
-        current_word = self.words[0]
+        # let starting word be a random selection from the first words
+        current_word = random.choice(self.first_words)
         output = []
         while current_word is not None:
             output.append(current_word)
+            # TODO: Change this random stuff to random.choice()
             randomIndex = random.randint(0, len(self.chains[current_word])-1)
             current_word = self.chains[current_word][randomIndex]
         return ' '.join(output)
